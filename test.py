75c7418404936d13f0c1cae5379e91d11d242f21
farajiptpb <mahdi.ptpb.faraji@gmail.com>
2022-06-21 15:34:02 +0430

    done(refactor needed)

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 primary = list()
-problems = ['33+44224', '22222+   99', '23423  - 3122']
+problems = ['3333+44224', '22222+   94322349', '23423  - 3122','89897-576869']
 
 for problem in problems:
     if '+' in problem:
@@ -37,13 +37,19 @@ for i in primary:
 d = dashes_row.split()
 first_operands_row = ''
 for num in first_operands:
-    fr_space_num = (len(d[first_operands.index(num)]) - len(num))*' '
+    index = first_operands.index(num)
+    fr_space_num = (len(d[index]) - len(num)) * ' '
     first_operands_row = first_operands_row + f"{fr_space_num}{num}{four_spaces}"
 
 
 second_operands_row = ''
 for num in second_operands:
-    sr_space_num = (len(d[second_operands.index(num)]) - len(num) - 1)*' '
-    second_operands_row = second_operands_row + f"+{sr_space_num}{num}{four_spaces}"
+    index = second_operands.index(num)
+    if '+' in problems[second_operands.index(num)]:
+        sign = '+'
+    else:
+        sign = '-'
+    sr_space_num = (len(d[index]) - len(num) - 1)*' '
+    second_operands_row = second_operands_row + f"{sign}{sr_space_num}{num}{four_spaces}"
 
 print(f"{first_operands_row}\n{second_operands_row}\n{dashes_row}")
